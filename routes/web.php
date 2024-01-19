3b6bdc7472c24af544465983754490e94043eab9
MrElle <cassaralavoro@gmail.com>
2024-01-19 15:37:47 +0100

    adding routes and fixing data

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,52 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//"characters" page link
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('characters');
+})->name('characters');
+
+//"comics" page link
+Route::get('comics', function () {
+    return view('comics', config('data.comics'));
+})->name('comics');
+
+//"movies" page link
+Route::get('movies', function () {
+    return view('movies');
+})->name('movies');
+
+//"tv" page link
+Route::get('tv', function () {
+    return view('tv');
+})->name('tv');
+
+//"games" page link
+Route::get('games', function () {
+    return view('games');
+})->name('games');
+
+//"collectibles" page link
+Route::get('collectibles', function () {
+    return view('collectibles');
+})->name('collectibles');
+
+//"videos" page link
+Route::get('videos', function () {
+    return view('videos');
+})->name('videos');
+
+//"fans" page link
+Route::get('fans', function () {
+    return view('fans');
+})->name('fans');
+
+//"news" page link
+Route::get('news', function () {
+    return view('news');
+})->name('news');
+
+//"shop" page link
+Route::get('shop', function () {
+    return view('shop');
+})->name('shop');
